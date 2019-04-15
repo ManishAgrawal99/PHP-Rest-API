914cc3d063d0342fad3c801d009dcce7c1e42088
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-04-15 13:02:14 +0530

    Created a PUT(update) and DELETE blog post API

--- models/Post.php
+++ models/Post.php
@@ -103,4 +103,69 @@ class Post
         printf("Error: %s.\n", $stmt->error);
 
     }
+
+    //Update Post
+    public function update(){
+        //Create Query
+        $query = 'UPDATE '. $this->table. ' 
+            SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id = :category_id
+            WHERE
+                id = :id
+        ';
+
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean Submitted Data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        //Bind Data
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
+
+        //Execute the query
+        if($stmt->execute()){
+            return true;
+        }
+
+        //Else print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+    }
+
+
+    //Delete Post
+    public function Delete(){
+        //Create query
+        $query = "DELETE FROM $this->table WHERE id = :id ";
+
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean Submitted Data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        //Bind Data
+        $stmt->bindParam(':id', $this->id);
+
+        //Execute the query
+        if($stmt->execute()){
+            return true;
+        }
+
+        //Else print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+        return false;
+    }
 }
